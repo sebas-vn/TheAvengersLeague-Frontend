af020f4620941e37bbbe9cfcf698f3d4bca2172a
Noah Gaston <nrgman@tds.net>
2021-10-08 16:05:11 -0500

    added llogin service and finished base register page

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -9,9 +9,15 @@ export class LoginService {
   public user = new User('','','','','');
   username: string = '';
   constructor(private userService: UserService) {  }
-  public findByUsername() 
+  public findByUsername() : boolean 
   {
+    let exists: boolean = true;
     this.userService.findByUsername(this.username)
       .subscribe(data => this.user = data)
+    if(this.user == null)
+    {
+      exists = false;
+    }
+    return exists;
   }
 }
