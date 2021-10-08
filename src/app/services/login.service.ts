07fc200cfe3e722511fb2a2e4ec7592a064760ee
Noah Gaston <nrgman@tds.net>
2021-10-08 15:59:32 -0500

    updated services

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   public user = new User('','','','','');
-  constructor(user: User) {
-    this.user = user;
+  username: string = '';
+  constructor(private userService: UserService) {  }
+  public findByUsername() 
+  {
+    this.userService.findByUsername(this.username)
+      .subscribe(data => this.user = data)
   }
 }
