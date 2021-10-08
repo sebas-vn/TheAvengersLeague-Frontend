7c3eb5cedeb8f4e41ef953516c0c27d40226fac0
Noah Gaston <nrgman@tds.net>
2021-10-08 10:50:37 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
