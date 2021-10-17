dc65c73363a79225245580ce9191e2bca0924a5c
Sebastian <sebasvn2340@gmail.com>
2021-10-16 21:55:11 -0400

    update on gameboard for heroes in users

--- src/app/components/dummyunit/dummyunit.component.ts
+++ src/app/components/dummyunit/dummyunit.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserInventory } from 'src/app/models/user-inventory';
 
 @Component({
   selector: 'app-dummyunit',
@@ -7,10 +11,35 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DummyunitComponent {
 
+  user: User = new User();
+  inventory: UserInventory;
+
   @Input('name') itemName;
   @Input('squareArray') squareArr;
   @Input() index;
   
-  constructor() { }
+  constructor(private userService:UserService, private router: Router) { }
+
+
+  ngOnInit(): void {
+
+    this.userService.getCurrent()
+    .subscribe(
+      data => {
+        if('email' in data.body) {
+          this.user = data.body;
+          this.userService.getInventory(this.user.id)
+            .subscribe(data => {
+              this.inventory = data.body;
+            });
+        } else {
+          this.router.navigate(['/front-door']);
+        }
+      },
+      error => this.router.navigate(['/front-door'])
+    );
+  }
+
+  
 
 }
