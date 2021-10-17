a31c6cbd7dd1ac2f69467e5459e0a62ad1e28f85
ImCoolYeah105 <maw23456789@gmail.com>
2021-10-17 12:33:28 -0400

    display power costs

--- src/app/components/game-play/game-play.component.ts
+++ src/app/components/game-play/game-play.component.ts
@@ -57,11 +57,15 @@ export class GamePlayComponent implements OnInit, OnDestroy {
   }
 
   playCard(index: number): void {
-    this.response.hand[index] = 0;
+    if(this.gameBoard.hand[index].powerCost <= this.gameBoard.power) {
+      this.response.hand[index] = 0;
+      this.response.power -= this.gameBoard.hand[index].powerCost;
+    }
   }
 
   unplayCard(index: number): void {
     this.response.hand[index] = this.gameBoard.hand[index].id;
+    this.response.power += this.gameBoard.hand[index].powerCost;
   }
 
   private handleStatus(status: string): void {
